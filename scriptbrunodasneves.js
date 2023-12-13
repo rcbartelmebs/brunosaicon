aca24a10a999816156f2c2b74ab9bd8d2536a0ef
Roberto Bartelmebs <68913942+rcbartelmebs@users.noreply.github.com>
2023-12-13 17:01:15 -0300

    first commit

--- scriptbrunodasneves.js
+++ scriptbrunodasneves.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
 
 // Configuração para receber dados no formato JSON
 app.use(express.json());
